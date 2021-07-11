3727dcef1a4c47021dd7831cd7d6ebaa1aa978f8
= <=>
2021-07-11 16:34:17 +0200

    added adc drivers to top level

--- verilog_source/rtl/gpio_reader.sv
+++ verilog_source/rtl/gpio_reader.sv
@@ -9,14 +9,137 @@ module gpio_reader
 	input wire [31:0] gpio_in,
 	
 	output reg [15:0] gpio_out,
-
 	
+	output wire valid,//1 if read was successful (if data was available in the a/c fifo)
+
+	//Inputs to be read out over gpio
+	input wire [15:0] del_meas_mac_result_in, del_meas_nl_result_in,
+	
+	input wire [num_bits-1:0] a_data,
+	input wire a_valid,
+	output reg a_ready,
+	
+	input wire [num_bits-1:0] c_data,
+	input wire c_valid,
+	output reg c_ready,
+	
+	input wire [127:0] mac_adc_data,
+	input wire mac_adc_valid,
+	output reg mac_adc_ready,
+	
+	input wire [127:0] nl_adc_data,
+	input wire nl_adc_valid,
+	output reg nl_adc_ready
 );
 
 
+wire w_clk = gpio_in[gpio_w_clk_bit];
+wire [addr_width-1:0] gpio_addr = gpio_in[gpio_addr_start:gpio_addr_end];
+wire [word_width-1:0] gpio_data = gpio_in[gpio_data_start:gpio_data_end];
 
+reg [15:0] ac_reg;
+reg ac_valid;
+wire a_data_wide = { {(16-num_bits){1'b0}}, a_data}; 
+wire c_data_wide = { {(16-num_bits){1'b0}}, c_data}; 
 
+wire reg_access = (gpio_addr == a_read_reg) || 
+				  (gpio_addr == c_read_reg) || 
+				  (gpio_addr == mac_adc_read_reg) || 
+				  (gpio_addr == nl_adc_read_reg);
+assign valid = reg_access ? ac_valid : 1;//Always valid if not accessing an axis bus
 
+reg [2:0] adc_cnt;
 
+task reset();
+begin
+	ac_reg <= 0;
+	ac_valid <= 0;
+	state <= 0;
+	valid <= 0;
+	a_ready <= 0;
+	c_ready <= 0;
+	adc_ready <= 0;
+end
+endtask
+
+initial begin
+	reset();
+end
+
+//fsm for reading ac back over 2 cycles
+always @ (posedge clk or negedge rst) begin
+	if(!rst) begin
+		reset();
+	end
+	else begin
+		if(!state) begin
+			if(w_clk) begin//Read a
+				if(gpio_addr == a_read_reg && a_valid) begin
+					ac_reg <= a_data_wide;
+					a_ready <= 1;
+					valid <= 1;
+				end//Read c
+				else if(gpio_addr == c_read_reg && c_valid) begin
+					ac_reg <= c_data_wide;
+					c_ready <= 1;
+					valid <= 1;
+				end
+				else if(gpio_addr == mac_adc_read_reg && mac_adc_valid) begin
+					//If this is the last read of the word
+					if(mac_adc_cnt == 3'b111) begin
+						mac_adc_ready <= 1;
+					end
+					ac_reg <= mac_adc_data[(mac_adc_cnt*16)+:16];
+					mac_adc_cnt <= mac_adc_cnt + 1;
+					valid <= 1;
+				end
+				else if(gpio_addr == nl_adc_read_reg && nl_adc_valid) begin
+					//If this is the last read of the word
+					if(nl_adc_cnt == 3'b111) begin
+						nl_adc_ready <= 1;
+					end
+					ac_reg <= nl_adc_data[(nl_adc_cnt*16)+:16];
+					nl_adc_cnt <= nl_adc_cnt + 1;
+					valid <= 1;
+				end
+			end
+		end
+		else begin//Reset everything and hold valid high until write goes low
+			a_ready <= 0;
+			c_ready <= 0;
+			adc_ready <= 0;
+			if(!w_clk) begin
+				state <= 0;
+				valid <= 0;
+			end
+		end
+	end
+end
+
+//Combo logic for selecting data
+always @ * begin
+
+	case(gpio_addr) begin
+	
+		a_read_reg: begin
+			gpio_out <= ac_reg;
+		end
+		c_read_reg: begin
+			gpio_out <= ac_reg;
+		end
+		
+		del_meas_mac_result: begin
+			gpio_out <= del_meas_mac_result_in;
+		end
+		del_meas_nl_result: begin
+			gpio_out <= del_meas_nl_result_in;
+		end
+		
+		default begin
+			gpio_out <= 0;
+		end
+	
+	endcase
+end
 
 endmodule
--- verilog_source/rtl/ising_config.sv
+++ verilog_source/rtl/ising_config.sv
@@ -3,26 +3,19 @@
 
 package ising_config;
 
+//GPIO bus definitions
 parameter gpio_w_clk_bit = 24;
 parameter gpio_addr_start = 15;
 parameter gpio_addr_end = 0;
 parameter gpio_data_start = 23;
 parameter gpio_data_end = 16;
-
 parameter gpio_addr_width = 16;
 parameter gpio_data_width = 8;
 
+//How many words the ADC can record
 parameter adc_buffer_len = 256;
 
 parameter num_bits = 8; //Bit precision to use for internal logic
-
-
-//Address table for configuration registers (0 to 65535)
-parameter mac_input_scaler_addr_reg = 0;
-parameter mac_input_scaler_data_reg = 0;
-parameter nl_input_scaler_addr_reg = 256;
-parameter nl_input_scaler_data_reg = 256;
-
 
 
 parameter var_fifo_depth = 12; //(2**12 = 4096)
@@ -33,6 +26,28 @@ parameter var_fifo_depth = 12; //(2**12 = 4096)
 //Config Reg Address table
 parameter run_trig_reg = 16'h0000;
 parameter del_trig_reg = 16'h0001;
+//Address table for configuration registers (0 to 65535)
+parameter mac_input_scaler_addr_reg = 16'h0002;
+parameter mac_input_scaler_data_reg = 16'h0003;
+parameter nl_input_scaler_addr_reg = 16'h0004;
+parameter nl_input_scaler_data_reg = 16'h0005;
+parameter mac_driver_addr_reg = 16'h000C;
+parameter mac_driver_data_reg = 16'h000D;
+parameter mac_driver_shift_amt_reg_base_addr = 16'h000E;
+parameter nl_driver_addr_reg = 16'h000F;
+parameter nl_driver_data_reg = 16'h0010;
+parameter nl_driver_shift_amt_reg_base_addr = 16'h0011;
+
+//Readback registers
+parameter del_meas_mac_result = 16'h0006;
+parameter del_meas_nl_result = 16'h0007;
+parameter a_read_reg = 16'h0008;
+parameter c_read_reg = 16'h0009;
+parameter mac_adc_read_reg = 16'h000A;
+parameter nl_adc_read_reg = 16'h000B;
+
+
+
 
 
 endpackage
